ba75d64dc50da3ec10581228bb4c1948e857edfa
Michal Špaček <tupinek@gmail.com>
2015-01-14 19:41:30 +0100

    First version.

---
+++ Reader.pm
@@ -0,0 +1,55 @@
+package Task::Graph::Reader;
+
+# Pragmas.
+use strict;
+use warnings;
+
+# Version.
+our $VERSION = 0.01;
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Task::Graph::Reader - Install the Graph::Reader modules.
+
+=head1 SYNOPSIS
+
+ cpanm Task::Graph::Reader
+
+=head1 SEE ALSO
+
+L<Graph::Reader>,
+L<Graph::Reader::OID>,
+L<Graph::Reader::TGF>,
+L<Graph::Reader::TGF::CSV>,
+L<Graph::Reader::UnicodeTree>,
+L<Graph::Reader::LoadClassHierarchy>.
+
+=head1 REPOSITORY
+
+L<https://github.com/tupinek/Task-Graph-Reader>
+
+=head1 AUTHOR
+
+Michal Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+ © 2015 Michal Špaček
+ Artistic License
+ BSD 2-Clause License
+
+=head1 VERSION
+
+0.01
+
+=cut
